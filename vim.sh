1877e903207f1659a4e7ddc3795c74122fe7dedb
bushman77 <banolik@gmail.com>
2018-09-12 11:04:30 -0700

    Create vim.sh

---
+++ vim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle git clone https://github.com/wkentaro/conque.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
