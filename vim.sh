e4188d1a3865d2aae7494836b444854a46bd574f
bushman77 <banolik@gmail.com>
2018-11-17 09:07:11 -0800

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -2,3 +2,6 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle && git clone https://github.com/wkentaro/conque.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+echo "colorscheme desert" >> ~/.vimrc
+echo "execute pathogen#infect()" >> ~/.vimrc
+echo "<C-n> :NERDTreeToggle<CR>" >> ~/.vimrc
