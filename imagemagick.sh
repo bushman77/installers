6d2448e90280dd50a2862604f44a5d8251c86757
bushman77 <banolik@gmail.com>
2018-11-21 17:34:34 -0800

    Update imagemagick.sh

--- imagemagick.sh
+++ imagemagick.sh
@@ -2,3 +2,17 @@
 
 #uncomment deb-src http://au.archive.ubuntu.com/ubuntu/ bionic main restricted
 
+sudo apt update
+sudo apt build-dep imagemagick
+
+wget https://www.imagemagick.org/download/ImageMagick.tar.gz
+tar xf ImageMagick.tar.gz
+cd ImageMagick-7*
+
+./configure
+
+make
+
+sudo make install
+
+sudo ldconfig /usr/local/lib
