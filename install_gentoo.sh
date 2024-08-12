9182854224e9f80c27bdab6b6393e9ec381ce7e3
bushman77 <banolik@gmail.com>
2024-08-12 15:40:50 +0000

    Update install_gentoo.sh

--- install_gentoo.sh
+++ install_gentoo.sh
@@ -7,8 +7,16 @@ parted -s /dev/sda mklabel gpt
 parted -s --align=optimal /dev/sda mkpart ESP fat32 1MiB 1Gib 
 parted -s /dev/sda set 1 esp on
 parted -a optimal /dev/sda1
+
 mkfs.vfat -F 32 /dev/sda1
 mkfs.xfs /dev/sda3
+
+mkswap /dev/sda2
+swapon /dev/sda2
+
+mkdir --parents /mnt/gentoo
+mkdir --parents /mnt/gentoo/efi
+mount /dev/sda3 /mnt/gentoo
 
 # Download stage 3 files
 
