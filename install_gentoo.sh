b39edcb057de759961f1b2d3ea41c58b975ee4e6
bushman77 <banolik@gmail.com>
2024-08-12 03:07:22 +0000

    Update install_gentoo.sh

--- install_gentoo.sh
+++ install_gentoo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo su
-sudo sfdisk -d /dev/sda > sda.sfdisk
-sfdisk -d /dev/sda > sda.sfdisk
+parted -a optimal /dev/sda1 mkpart primary 0% 4096MB
 # Download stage 3 files
 
 echo "sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE" | tee -a /etc/portage/package.license 
