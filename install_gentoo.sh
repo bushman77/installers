4b1a49f82b3c07d4ad0f944156bc93ab0db2c7d5
bushman77 <banolik@gmail.com>
2024-08-12 03:27:35 +0000

    Update install_gentoo.sh

--- install_gentoo.sh
+++ install_gentoo.sh
@@ -2,12 +2,11 @@
 
 ## SETUP PARTITIONS
 sudo su
-  parted -s /dev/sda mklabel gpt
-  parted -s --align=optimal $disk mkpart ESP fat32 1MiB 1Gib 
-  parted -s $disk set 1 esp on
-  #parted -s $disk set 1 bios_grub on
-  parted -s --align=optimal $disk mkpart btrfs 1Gib 100%
-  parted -a optimal /dev/sda1 mkpart primary 0% 1024MB
+parted -s /dev/sda mklabel gpt
+parted -s --align=optimal /dev/sda mkpart ESP fat32 1MiB 1Gib 
+parted -s /dev/sda set 1 esp on
+parted -a optimal /dev/sda1 mkpart primary 0% 1024MB
+  
 # Download stage 3 files
 
 echo "sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE" | tee -a /etc/portage/package.license 
