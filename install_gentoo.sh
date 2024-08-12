6e415723209946f62296af85ffb882100c06dd69
bushman77 <banolik@gmail.com>
2024-08-12 03:21:34 +0000

    Update install_gentoo.sh

--- install_gentoo.sh
+++ install_gentoo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo su
-parted -a optimal /dev/sda1 mkpart primary 0% 4096MB
+parted -a optimal /dev/sda1 mkpart primary 0% 1024MB
 # Download stage 3 files
 
 echo "sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE" | tee -a /etc/portage/package.license 
